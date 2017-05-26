8e8de08c85a254d9fc9266b1ab70a589ebaf65a6
smithm <martin.smith@dowjones.com>
2017-05-26 11:56:53 +0100

    Add Error modal

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,8 @@ module.exports = {
       About: 'app/components/About.jsx',
       Examples: 'app/components/Examples.jsx',
       Nav:  'app/components/Nav.jsx',
-      openWeatherMap: 'app/api/openWeatherMap.jsx'
+      openWeatherMap: 'app/api/openWeatherMap.jsx',
+      ErrorModal: 'app/components/ErrorModal.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
